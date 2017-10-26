832179296959277b3758aa537b79ba476b6205a7
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-10-26 14:32:19 -0400

    first commit

---
+++ youtubeplayer.rb
@@ -0,0 +1,49 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'youtube-dl.rb'
+require 'streamio-ffmpeg'
+
+class Track
+
+  def initialize(link)
+    @link = link
+  end
+
+  def video_details
+    page = Nokogiri::HTML(open(@link))
+    return page.title.gsub(/ - YouTube/, '')
+  end
+
+  def file_path
+    "/Users/brycewendelaar/Desktop/_Exports/#{video_details}"
+  end
+
+  def download_video
+    options = {
+      extract_audio: "true",
+      audio_format: "mp3",
+      output: file_path + ".%(ext)s"
+    }
+    puts "Downloading..."
+    YoutubeDL.download @link, options
+    puts "Audio Extracted"
+  end
+
+  def print_link
+    "Your Link = #{@link}"
+  end
+
+end
+
+loop do
+  puts "Paste an audio file... ('x' to quit)"
+  track = gets.chomp
+  if (track == "x")
+    puts "Goodbye!"
+    break
+  else
+    new_track = Track.new(track)
+    new_track.download_video
+  end
+end
